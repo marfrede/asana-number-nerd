ecebc0a3f3a1ae5bd0c0bb78579681b5ac81bcc4
marvinfrede <marvinfrede@gmx.de>
2023-01-19 19:22:22 +0100

    bugfix for: move asana classes from classes/asana to asana/classes

--- mymodules/asana/functions/auth.py
+++ mymodules/asana/functions/auth.py
@@ -6,20 +6,20 @@ import requests
 from deta import Deta
 from fastapi import Request
 
-from classes.asana import Token, TokenNoRefresh, User
-from classes.local_env import Env
+from mymodules import environment
+from mymodules.asana import classes
 
 # init deta databse
 deta = Deta()
 db = deta.Base("ann_db")  # This how to connect to or create a database.
 
 
-def refresh_pat(request: Request, env: Env) -> Tuple[Union[User, None], Union[str, None]]:
+def refresh_pat(request: Request, env: environment) -> Tuple[Union[classes.User, None], Union[str, None]]:
     '''read user id from session and read user from detabase'''
     asana_user_id: Union[str, None] = request.session.get("asana_user_id", None)
     if not asana_user_id:
         return (None, None)
-    access_token: Union[Token, None] = db.get(f"user_{asana_user_id}")
+    access_token: Union[classes.Token, None] = db.get(f"user_{asana_user_id}")
     if not access_token:
         return (None, None)
     pat: Union[str, None] = __refresh_asana_client_pat(access_token=access_token, env=env)
@@ -28,7 +28,7 @@ def refresh_pat(request: Request, env: Env) -> Tuple[Union[User, None], Union[st
 
 # HELPER
 
-def __refresh_asana_client_pat(access_token: Token, env: Env) -> Union[str, None]:
+def __refresh_asana_client_pat(access_token: classes.Token, env: environment.Env) -> Union[str, None]:
     '''
         refresh asana access_token (pat) with refresh_token
         save new access_token object in db
@@ -43,7 +43,7 @@ def __refresh_asana_client_pat(access_token: Token, env: Env) -> Union[str, None
         'refresh_token': access_token["refresh_token"]
     }, timeout=5)
     if (response.status_code >= 200 and response.status_code < 400):
-        new_access_token: TokenNoRefresh = response.json()
+        new_access_token: classes.TokenNoRefresh = response.json()
         pat: str = new_access_token["access_token"]
         access_token["access_token"] = pat
         db.put(access_token, f"user_{access_token['data']['id']}")
