83b20dfd81d605a89e8971f33db4c7f5d9ccc805
marvinfrede <marvinfrede@gmx.de>
2023-01-21 07:55:23 +0100

    explicitly use detabase inside deta module and reactivate choose-numbering page

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 import ast
 from typing import Any, Coroutine, Dict, List, Union
 
-from deta import Deta
 from fastapi import Depends, FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette import status as Status
@@ -20,10 +19,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # init jinja templates
 templates = Jinja2Templates(directory="templates")
-
-# init deta databse
-deta_project = Deta()
-detabase = deta_project.Base("ann_db")  # This how to connect to or create a database.
 
 
 @ app.get("/home", response_class=RedirectResponse)
@@ -63,11 +58,11 @@ async def oauth_callback(
 
     # fetch auth_token for user
     access_token: asana.Token = asana.oauth.get_client(env).session.fetch_token(code=code)
+    asana_user_id: str = access_token['data']["id"]
 
     # store auth_token in db and db key in session
-    asana_user_id: str = access_token['data']["id"]
     request.session['asana_user_id'] = asana_user_id
-    deta.put_access_token(asana_user_id, access_token=access_token)
+    deta.put_access_token(asana_user_id=asana_user_id, access_token=access_token)
     return RedirectResponse("/choose-projects")
 
 
@@ -75,12 +70,12 @@ async def oauth_callback(
 async def choose_projects(request: Request, env: environment.Env = Depends(environment.get_env)):
     '''site for the authenticated user'''
     # 1. auth or redirect
-    access_token: Union[asana.Token, None] = read_access_token_from_db(session=request.session)
-    access_token, asana_user, pat = asana.auth.refresh_token(old_access_token=access_token, env=env)
+    user: Union[deta.User, None] = read_user_from_db(session=request.session)
+    access_token, asana_user, pat = asana.auth.refresh_token(old_access_token=user["access_token"], env=env)
     if (not access_token):
         return RedirectResponse("/")
     # 2. save new acess_token and respond
-    detabase.put(access_token, f"user_{access_token['data']['id']}")
+    deta.put_access_token(asana_user_id=access_token['data']['id'], access_token=access_token)
     workspaces: List[asana.Object] = asana.http.get(url="workspaces", pat=pat)
     for workspace in workspaces:
         projects: List[asana.Object] = asana.http.get(url=f"workspaces/{workspace['gid']}/projects", pat=pat)
@@ -92,72 +87,67 @@ async def choose_projects(request: Request, env: environment.Env = Depends(envir
     })
 
 
-@ app.post("/projects/read", response_class=RedirectResponse)
-async def read_projects(request: Request):
-    '''read chosen projects from form and save to detabase'''
+@app.post("/choose-numbering", response_class=HTMLResponse)
+async def choose_numbering(request: Request, env: environment.Env = Depends(environment.get_env)):
+    '''site for the authenticated user'''
+    # read projects from form
     projects: List[asana.Object] = await read_projects_from_form(request=request)
-    deta_obj = detabase.put(projects)
-    request.session["projects_choosen"] = deta_obj["key"]
-    return RedirectResponse("/webhook/create")
-
-
-# @app.get("/choose-numbering", response_class=HTMLResponse)
-# async def choose_numbering(request: Request, env: Env = Depends(get_env)):
-#     '''site for the authenticated user'''
-#     # 1. auth and validate or redirect
-#     asana_user, _ = get_fresh_logged_in_asana_user(request=request, env=env)
-#     projects = await read_projects_session_db(request=request)
-#     if not asana_user or not projects:
-#         return RedirectResponse("/choose-projects")
-#     # 2. respond
-#     # return templates.TemplateResponse("choose-numbering.jinja2", {
-#     #     "request": request,
-#     #     "asana_user": asana_user,
-#     #     "projects": projects,
-#     # })
-#     return RedirectResponse("/choose-numbering", status_code=Status.HTTP_302_FOUND)
-
-
-@ app.post("/webhook/create")
-async def create_weebhook(request: Request, env: environment.Env = Depends(environment.get_env)):
-    '''create the webhook to listen to create-task events inside given projects'''
-    # 1. auth and validate or redirect
-    access_token: Union[asana.Token, None] = read_access_token_from_db(session=request.session)
-    access_token, _, pat = asana.auth.refresh_token(old_access_token=access_token, env=env)
-    # 2. save new acess_token and respond
-    detabase.put(access_token, f"user_{access_token['data']['id']}")
-    projects: Union[List[asana.Object], None] = await read_projects_session_db(request=request, delete_after_read=True)
-    if not pat or not projects:
+    user: Union[deta.User, None] = deta.put_projects(asana_user_id=request.session.get("asana_user_id", None), projects=projects)
+    if not user:
         return RedirectResponse("/choose-projects")
-    response = asana.http.post(
-        url="webhooks", pat=pat,
-        data=asana.webhooks.post_request_body(
-            project_gid=projects[0]["gid"],
-            callback_url=env.number_nerd_webhook_callback),
-    )
-    if (response.status_code >= 200 and response.status_code < 400):
-        return response.json()["data"]
-    return response.json()
+    # 1. auth and validate or redirect
+    _, asana_user, _ = asana.auth.refresh_token(old_access_token=user["access_token"], env=env)
+    if not asana_user:
+        return RedirectResponse("/choose-projects")
+    # 2. respond
+    return templates.TemplateResponse("choose-numbering.jinja2", {
+        "request": request,
+        "asana_user": asana_user,
+        "projects": projects,
+    })
+    # return RedirectResponse("/choose-numbering", status_code=Status.HTTP_302_FOUND)
 
 
-@app.post("/webhook/receive")
-async def receive_weebhook(request: Request, response: Response):
-    '''callback for asana when task created (and for first handshake)'''
-    pat = "1/1199181200186785:d6752d0cc04c304e22d12e0b57163c14"
-    secret: Union[str, None] = request.headers.get("X-Hook-Secret")
-    if secret:
-        # db.put(secret, f"x_hook_secret_{user_gid}_{project_gid}")
-        response.status_code = Status.HTTP_204_NO_CONTENT
-        response.headers["X-Hook-Secret"] = secret
-        return None
-    # create a task
-    body: dict = await request.json()
-    task_created_gid: str = body["events"][0]["resource"]["gid"]
-    task_created_name = asana.http.get(url=f"tasks/{task_created_gid}", pat=pat,)["name"]
-    asana.http.put(
-        url=f"tasks/{task_created_gid}", pat=pat,
-        json={"data": {"name": f"{'1'} {task_created_name}"}}
-    )
+# @ app.post("/webhook/create")
+# async def create_weebhook(request: Request, env: environment.Env = Depends(environment.get_env)):
+#     '''create the webhook to listen to create-task events inside given projects'''
+#     # 1. auth and validate or redirect
+#    user: Union[deta.User, None] = read_user_from_db(session=request.session)
+#     access_token, _, pat = asana.auth.refresh_token(old_access_token=user["access_token"], env=env)
+#     # 2. save new acess_token and respond
+#    # detabase.put(access_token, f"user_{access_token['data']['id']}")
+#     projects: Union[List[asana.Object], None] = await read_projects_session_db(request=request, delete_after_read=True)
+#     if not pat or not projects:
+#         return RedirectResponse("/choose-projects")
+#     response = asana.http.post(
+#         url="webhooks", pat=pat,
+#         data=asana.webhooks.post_request_body(
+#             project_gid=projects[0]["gid"],
+#             callback_url=env.number_nerd_webhook_callback),
+#     )
+#     if (response.status_code >= 200 and response.status_code < 400):
+#         return response.json()["data"]
+#     return response.json()
+
+
+# @app.post("/webhook/receive")
+# async def receive_weebhook(request: Request, response: Response):
+#     '''callback for asana when task created (and for first handshake)'''
+#     pat = "1/1199181200186785:d6752d0cc04c304e22d12e0b57163c14"
+#     secret: Union[str, None] = request.headers.get("X-Hook-Secret")
+#     if secret:
+#         # db.put(secret, f"x_hook_secret_{user_gid}_{project_gid}")
+#         response.status_code = Status.HTTP_204_NO_CONTENT
+#         response.headers["X-Hook-Secret"] = secret
+#         return None
+#     # create a task
+#     body: dict = await request.json()
+#     task_created_gid: str = body["events"][0]["resource"]["gid"]
+#     task_created_name = asana.http.get(url=f"tasks/{task_created_gid}", pat=pat,)["name"]
+#     asana.http.put(
+#         url=f"tasks/{task_created_gid}", pat=pat,
+#         json={"data": {"name": f"{'1'} {task_created_name}"}}
+#     )
 
 
 # HELPER
@@ -171,24 +161,24 @@ async def read_projects_from_form(request: Request) -> Coroutine[List[asana.Obje
     return projects
 
 
-async def read_projects_session_db(
-    request: Request,
-    delete_after_read: bool = False
-) -> Coroutine[Union[List[asana.Object], None], None, None]:
-    '''read project ids selected inside form after storing in db'''
-    key: Union[str, None] = request.session.get("projects_choosen")
-    if not key:
-        return None
-    projects_choosen: Union[List[asana.Object], None] = detabase.get(key)["value"]
-    if not projects_choosen:
-        return None
-    if delete_after_read:
-        detabase.delete(key)
-        request.session.pop("projects_choosen")
-    return projects_choosen
+# async def read_projects_session_db(
+#     request: Request,
+#     delete_after_read: bool = False
+# ) -> Coroutine[Union[List[asana.Object], None], None, None]:
+#     '''read project ids selected inside form after storing in db'''
+#     key: Union[str, None] = request.session.get("projects_choosen")
+#     if not key:
+#         return None
+#     projects_choosen: Union[List[asana.Object], None] = detabase.get(key)["value"]
+#     if not projects_choosen:
+#         return None
+#     if delete_after_read:
+#         detabase.delete(key)
+#         request.session.pop("projects_choosen")
+#     return projects_choosen
 
 
-def read_access_token_from_db(session: Dict[str, Any]) -> Union[asana.Token, None]:
+def read_user_from_db(session: Dict[str, Any]) -> Union[deta.User, None]:
     '''
         read user_id from session and then asana_access_token from db
         return None if user_id not found in session or access_token not found in db
@@ -196,5 +186,5 @@ def read_access_token_from_db(session: Dict[str, Any]) -> Union[asana.Token, Non
     asana_user_id: Union[str, None] = session.get("asana_user_id", None)
     if not asana_user_id:
         return None
-    access_token: Union[asana.Token, None] = detabase.get(f"user_{asana_user_id}")
-    return access_token
+    user: deta.User = deta.get_user(asana_user_id)
+    return user
--- modules/deta/functions/operations.py
+++ modules/deta/functions/operations.py
@@ -1,20 +1,52 @@
 '''detabase operations'''
+
+
+from typing import List, Union
+
 from deta import Deta
 
 from modules import asana
 
-# from ..classes import User
+from ..classes import User
 
 # init deta databse
 deta = Deta()
 detabase = deta.Base("ann_db")  # This how to connect to or create a database.
 
 
-def put_access_token(asana_user_id: str, access_token: asana.Token) -> None:
-    '''store and update users access_token'''
-    detabase.put(access_token, __get_key(asana_user_id))
-    return
+def get_user(asana_user_id: str) -> User:
+    '''get user and all their values'''
+    user: Union[User, None] = detabase.get(key=__get_key(asana_user_id))
+    if not user:
+        return {
+            "access_token": None,
+            "projects": [],
+            "webhooks": []
+        }
+    return user
+
+
+def put_access_token(asana_user_id: str, access_token: asana.Token) -> User:
+    '''store and update access_token to user["access_token"]'''
+    user: User = get_user(asana_user_id)
+    user["access_token"] = access_token
+    __store_user(asana_user_id, user)
+    return user
+
+
+def put_projects(asana_user_id: str, projects: List[asana.Object]) -> User:
+    '''store and update projects to user["projects"]'''
+    user: User = get_user(asana_user_id)
+    user["projects"] = projects
+    __store_user(asana_user_id, user)
+    return user
+
+
+def __store_user(asana_user_id: str, user: User) -> None:
+    '''overwrite user object in detabase'''
+    detabase.put(user, key=__get_key(asana_user_id))
 
 
 def __get_key(asana_user_id: str) -> str:
+    '''create key string using asana_user_id to identify detabase user'''
     return f"user_{asana_user_id}"
